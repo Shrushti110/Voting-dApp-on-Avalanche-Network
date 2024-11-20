ff87706dc5e181478ace7714eb391172884cc78f
Shrushti110 <giramkarshrushti@gmail.com>
2024-11-20 13:10:44 +0530

    new file created

--- pages/Navbar.jsx
+++ pages/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = ({ account, connectWallet }) => {
     <nav className="flex items-center justify-between flex-wrap bg-gradient-to-r from-gray-900 to-gray-700 p-6">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
         <span className="font-bold text-3xl tracking-tight">
-          Web3 Voting DAPP
+         Decentralized Voting DAPP
         </span>
       </div>
       <div className="flex">
@@ -29,9 +29,7 @@ const Navbar = ({ account, connectWallet }) => {
     </nav>
   )
 }
-
 export default Navbar
-
 /*
 Don't forget to pass the parameters account and setAccount in app.js while importing
 */
